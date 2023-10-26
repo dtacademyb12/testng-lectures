07065152d1d80e0ba361d0670b52e26431804782
Duotech B12 <duotechb12@gmail.com>
2023-10-25 22:07:00 -0400

    surefire plugin configured

--- src/test/java/ui/DemoTest.java
+++ src/test/java/ui/DemoTest.java
@@ -6,14 +6,13 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class Demo {
+public class DemoTest {
 
 
     WebDriver driver;
